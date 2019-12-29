17f75e5566e10ffb2be7ddbc846638df1d4c0a1a
roodrigoroot <roodrigoroot@gmail.com>
2019-12-29 16:31:56 -0600

    Try-except added

--- Main.py
+++ Main.py
@@ -1,18 +1,29 @@
-#from Weekly.Week import Weeks
-#from Monthly.Monthly import Months
+from weekly.Week import Weeks
+from monthly.Monthly import Months
 from Reports.Report import Reports
 
 class Main:
+
     
-    def __init__(self):
-        
-        pass
     @classmethod
     def semana(cls):
-        #week1 = Weeks(int(input("Semanas pagadas del primer mes: ")))
-        #week2 = Weeks(int(input("Semanas pagadas del segundo mes: ")))
-        #month=Months(week1.sum_weeks(), week2.sum_weeks_2())
-        Reports.generate_Report()
+        while True:
+                
+            try:
+                    
+                week1 = Weeks(int(input("Semanas pagadas del primer mes: ")))
+                week2 = Weeks(int(input("Semanas pagadas del segundo mes: ")))
+                month=Months(week1.sum_weeks(), week2.sum_weeks_2())
+                if month:
+                    Reports.generate_Report()
+                    break
+                
+            except ValueError:
+                print("Solo aceptamos numeros")
+            except KeyboardInterrupt:
+                print("No puedes terminar la App")
+            except Exception as e:
+                print(e.__class__)
 
 if __name__=="__main__":
     Main.semana()
--- weekly/Week.py
+++ weekly/Week.py
@@ -4,16 +4,21 @@ class Weeks:
     def __init__(self, payments):
         self.__MOTHLY_TOTAL_1 = 0
         self.__MOTHLY_TOTAL_2 = 0
-        self.payments = payments
+        self.payments = 
         
 
     def __str__(self):
         return "Weeks"
 
     def sum_weeks(self):
-        print("Mes 1")
-        for i in range(self.payments):
-           self.__MOTHLY_TOTAL_1 += self.obtain_Monthly(i)
+        if self.payments:
+            print("Mes 1")
+            for i in range(self.payments):
+                self.__MOTHLY_TOTAL_1 += self.obtain_Monthly(i)
+        else:
+            raise ValueError("""No se ha asignado
+                             el valor de los pagons mensuales del primer mes """)
+            
         return self.__MOTHLY_TOTAL_1
 
     def sum_weeks_2(self):
